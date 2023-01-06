8a9ffaebafdabca930fa1e4d1b18a7f74f9019d9
leolukaz123 <122065825+leolukaz123@users.noreply.github.com>
2023-01-06 13:54:42 +1100

    changed stuff

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = (props) => {
         <div className="home-hero">
           <span className="home-text">Easy Equity</span>
           <h1 className="home-text01">
-            Easily unlock your housing equity again
+            Easily unlock your housing equity again cats
           </h1>
           <Link href="/pre-qualification-confirm-address">
             <a className="home-link">
